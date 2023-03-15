f4c457b7883bbd915fa00955dd286f38d04e298c
unknown <ziborova.01@mail.ru>
2023-03-15 16:35:56 +0400

    add solution

---
+++ OnlineAuction.BLL/BllModule.cs
@@ -0,0 +1,14 @@
+﻿using Ninject.Modules;
+using OnlineAuction.BLL.Interface;
+
+namespace OnlineAuction.BLL
+{
+    public class BllModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IUserLogic>().To<UserLogic>();
+            Bind<ILotLogic>().To<LotLogic>();
+        }
+    }
+}
---
+++ OnlineAuction.DAL/DalModule.cs
@@ -0,0 +1,14 @@
+﻿using Ninject.Modules;
+using OnlineAuction.DAL.Interface;
+
+namespace OnlineAuction.DAL
+{
+    public class DalModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<ILotDao>().To<LotDao>();
+            Bind<IUserDao>().To<UserDao>();
+        }
+    }
+}
